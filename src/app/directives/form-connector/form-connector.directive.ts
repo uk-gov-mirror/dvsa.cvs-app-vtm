dvsa/cvs-app-vtm

feat(cvsb-10184): lose changes modal  with common logic for all modals

src/app/directives/form-connector/form-connector.directive.ts
@@ -1,7 +1,13 @@
 import { Directive, Input, OnInit, OnDestroy } from '@angular/core';
-import { FormGroupDirective } from '@angular/forms';
+import { FormGroupDirective, FormGroup } from '@angular/forms';
 import { Subscription } from 'rxjs';
-import { debounceTime, tap } from 'rxjs/operators';
+import { debounceTime, withLatestFrom, map } from 'rxjs/operators';
+import { Store } from '@ngrx/store';
+import { IAppState } from '@app/store/state/app.state';
+import { SetAppFormDirty } from '@app/store/actions/app-form-state.actions';
+import { getAppFormState } from '../../store/selectors/app-form-state.selectors';
+import { getTechViewState } from '@app/store/selectors/VehicleTechRecordModel.selectors';
+import { VIEW_STATE } from '@app/app.enums';
 
 @Directive({
   selector: '[vtmFormConnector]'
@@ -16,16 +22,25 @@ export class FormConnectorDirective implements OnInit, OnDestroy {
   formSuccess: Subscription;
   formError: Subscription;
 
-  constructor(private formGroupDirective: FormGroupDirective) {}
+  constructor(private formGroupDirective: FormGroupDirective, private store: Store<IAppState>) {}
 
   ngOnInit(): void {
     this.formChange = this.formGroupDirective.form.valueChanges
       .pipe(
         debounceTime(this.debounce),
-        tap(value => console.log('VALUE: ', value, 'PATH: ', this.path))
+        withLatestFrom(this.store.select(getTechViewState), this.store.select(getAppFormState)),
+        map(([forValue, viewState, appForm]: [FormGroup, VIEW_STATE, boolean]) => {
+          // tap((value) => {
+          console.log('VALUE: ', forValue, 'PATH: ', this.path);
+          if (!this.formGroupDirective.form.pristine && viewState !== VIEW_STATE.VIEW_ONLY && appForm) {
+            //     console.log('firing app form dirty');
+            this.store.dispatch(new SetAppFormDirty());
+          }
+          // })
+        })
       )
       .subscribe();
-      console.log(this.formGroupDirective.form);
+    console.log(this.formGroupDirective.form);
   }
 
   ngOnDestroy(): void {

uk-gov-mirror/dvsa.cvs-app-vtm

src/app/directives/form-connector/form-connector.directive.ts
@@ -1,7 +1,11 @@
 import { Directive, Input, OnInit, OnDestroy } from '@angular/core';
-import { FormGroupDirective } from '@angular/forms';
+import { FormGroupDirective, FormGroup } from '@angular/forms';
 import { Subscription } from 'rxjs';
-import { debounceTime, tap } from 'rxjs/operators';
+import { debounceTime, withLatestFrom, tap } from 'rxjs/operators';
+import { Store } from '@ngrx/store';
+import { IAppState } from '@app/store/state/app.state';
+import { SetAppFormDirty } from '@app/store/actions/app-form-state.actions';
+import { getAppFormState } from '../../store/selectors/app-form-state.selectors';
 
 @Directive({
   selector: '[vtmFormConnector]'
@@ -16,16 +20,24 @@ export class FormConnectorDirective implements OnInit, OnDestroy {
   formSuccess: Subscription;
   formError: Subscription;
 
-  constructor(private formGroupDirective: FormGroupDirective) {}
+  constructor(private formGroupDirective: FormGroupDirective, private store: Store<IAppState>) {}
 
   ngOnInit(): void {
     this.formChange = this.formGroupDirective.form.valueChanges
       .pipe(
         debounceTime(this.debounce),
-        tap(value => console.log('VALUE: ', value, 'PATH: ', this.path))
+        withLatestFrom(this.store.select(getAppFormState)),
+        tap(([formValue, appForm]: [FormGroup, boolean]) => {
+          // TODO: to be removed
+          console.log('VALUE: ', formValue, 'PATH: ', this.path);
+          if (!this.formGroupDirective.form.pristine && appForm) {
+            this.store.dispatch(new SetAppFormDirty());
+          }
+        })
       )
       .subscribe();
-      console.log(this.formGroupDirective.form);
+    // TODO: to be removed
+    console.log(this.formGroupDirective.form);
   }
 
   ngOnDestroy(): void {