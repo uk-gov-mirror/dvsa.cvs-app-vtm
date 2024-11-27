163a90a596d5160ec9fe5f137909f63ecd36e399
pbardy2000 <146740183+pbardy2000@users.noreply.github.com>
2024-11-27 14:45:02 +0000

    feat(CB2-14451): convert to old syntax

--- src/app/forms/components/govuk-checkbox-group/govuk-checkbox-group.component.ts
+++ src/app/forms/components/govuk-checkbox-group/govuk-checkbox-group.component.ts
@@ -1,11 +1,13 @@
+import { CustomTag } from '@/src/app/services/dynamic-forms/dynamic-form.types';
 import { CommonModule } from '@angular/common';
-import { Component, computed, forwardRef, inject, input, model, output } from '@angular/core';
+import { Component, EventEmitter, Input, Output, forwardRef, inject } from '@angular/core';
 import { ControlContainer, ControlValueAccessor, FormsModule, NG_VALUE_ACCESSOR } from '@angular/forms';
+import { SharedModule } from '../../../shared/shared.module';
 
 @Component({
 	selector: 'govuk-checkbox-group',
 	standalone: true,
-	imports: [CommonModule, FormsModule],
+	imports: [CommonModule, FormsModule, SharedModule],
 	templateUrl: './govuk-checkbox-group.component.html',
 	styleUrls: ['./govuk-checkbox-group.component.scss'],
 	providers: [
@@ -17,23 +19,64 @@ import { ControlContainer, ControlValueAccessor, FormsModule, NG_VALUE_ACCESSOR 
 	],
 })
 export class GovukCheckboxGroupComponent implements ControlValueAccessor {
-	blur = output<FocusEvent>();
-	focus = output<FocusEvent>();
-	value = model<unknown[] | null>(null);
-	disabled = model<boolean>(false);
-	options = input.required<{ value: any; label: string }[]>();
-	controlHint = input<string>('', { alias: 'hint' });
-	controlName = input<string>('', { alias: 'formControlName' });
-	controlLabel = input<string>('', { alias: 'label' });
+	@Output() blur = new EventEmitter<FocusEvent>();
+	@Output() focus = new EventEmitter<FocusEvent>();
+
+	@Input()
+	value: unknown[] | null = null;
+
+	@Input()
+	disabled = false;
+
+	@Input()
+	tags: CustomTag[] = [];
+
+	@Input({ required: true })
+	options!: { value: any; label: string }[];
+
+	@Input({ alias: 'hint' })
+	controlHint = '';
+
+	@Input({ alias: 'formControlName' })
+	controlName = '';
+
+	@Input({ alias: 'label' })
+	controlLabel = '';
+
+	@Input({ alias: 'id' })
+	controlId = '';
+
 	controlContainer = inject(ControlContainer);
-	control = computed(() => this.controlContainer.control?.get(this.controlName()));
-	controlId = input<string>('', { alias: 'id' });
+
+	get control() {
+		return this.controlContainer.control?.get(this.controlName);
+	}
+
+	get id() {
+		return this.controlId || this.controlName;
+	}
+
+	get hintId() {
+		return `${this.id}-hint`;
+	}
+
+	get labelId() {
+		return `${this.id}-label`;
+	}
+
+	get errorId() {
+		return `${this.id}-error`;
+	}
+
+	get hasError() {
+		return this.control?.invalid && this.control?.touched && this.control?.errors;
+	}
 
 	onChange = (_: any) => {};
 	onTouched = () => {};
 
 	writeValue(obj: any): void {
-		this.value.set(obj);
+		this.value = obj;
 		this.onChange(obj);
 	}
 
@@ -46,25 +89,25 @@ export class GovukCheckboxGroupComponent implements ControlValueAccessor {
 	}
 
 	setDisabledState?(isDisabled: boolean): void {
-		this.disabled.set(isDisabled);
+		this.disabled = isDisabled;
 	}
 
 	toggle(option: any) {
-		const value = this.value();
+		const value = this.value;
 
 		if (!value) {
-			this.value.set([option]);
-			this.onChange(this.value());
+			this.value = [option];
+			this.onChange(this.value);
 			return;
 		}
 
 		const arr = [...value];
 		arr.includes(option) ? arr.splice(arr.indexOf(option), 1) : arr.push(option);
-		this.value.set(arr);
-		this.onChange(this.value());
+		this.value = arr;
+		this.onChange(this.value);
 	}
 
 	isChecked(value: unknown) {
-		return this.value()?.includes(value?.toString());
+		return this.value?.includes(value?.toString());
 	}
 }
