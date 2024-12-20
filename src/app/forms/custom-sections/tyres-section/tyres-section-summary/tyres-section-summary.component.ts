009d73f6685957cb83ce4c6174d45b299bf22922
Brandon Thomas-Davies <87308252+BrandonT95@users.noreply.github.com>
2024-12-20 14:21:40 +0000

    feat(cb2-14452): add summary progress

--- src/app/forms/custom-sections/tyres-section/tyres-section-summary/tyres-section-summary.component.ts
+++ src/app/forms/custom-sections/tyres-section/tyres-section-summary/tyres-section-summary.component.ts
@@ -1,6 +1,7 @@
 import { TechnicalRecordService } from '@/src/app/services/technical-record/technical-record.service';
 import { Component, inject } from '@angular/core';
-import { TechRecordType } from '@dvsa/cvs-type-definitions/types/v3/tech-record/tech-record-verb';
+import { TechRecordType } from '@dvsa/cvs-type-definitions/types/v3/tech-record/tech-record-vehicle-type';
+import { TechRecordType as TechRecordTypeVerb } from '@dvsa/cvs-type-definitions/types/v3/tech-record/tech-record-verb';
 import { VehicleTypes } from '@models/vehicle-tech-record.model';
 import { Store } from '@ngrx/store';
 import { editingTechRecord, techRecord } from '@store/technical-records';
@@ -25,6 +26,17 @@ export class TyresSectionSummaryComponent {
 		const amended = this.amendedTechRecord();
 		if (!current || !amended) return true;
 
-		return !isEqual(current[property as keyof TechRecordType<'put'>], amended[property as keyof TechRecordType<'put'>]);
+		return !isEqual(
+			current[property as keyof TechRecordTypeVerb<'put'>],
+			amended[property as keyof TechRecordTypeVerb<'put'>]
+		);
+	}
+
+	hasAxleChanged(index: number): boolean {
+		const currentAxle = (this.currentTechRecord() as TechRecordType<'hgv' | 'psv' | 'trl'>)?.techRecord_axles?.[index];
+		const amendedAxle = (this.amendedTechRecord() as TechRecordType<'hgv' | 'psv' | 'trl'>)?.techRecord_axles?.[index];
+		if (!currentAxle || !amendedAxle) return true;
+
+		return !isEqual(currentAxle, amendedAxle);
 	}
 }
