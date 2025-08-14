cef6367baf45430bc8c99bb6a029919000bbe957
pbardy2000 <146740183+pbardy2000@users.noreply.github.com>
2025-08-14 10:31:36 +0100

    feat(CB2-18827): populate summary pane

--- src/app/features/tech-record/create/components/hydrate-new-vehicle-record-wrapper/hydrate-new-vehicle-record-v2/hydrate-new-vehicle-record-v2.component.ts
+++ src/app/features/tech-record/create/components/hydrate-new-vehicle-record-wrapper/hydrate-new-vehicle-record-v2/hydrate-new-vehicle-record-v2.component.ts
@@ -1,10 +1,15 @@
 import { AccordionControlComponent } from '@/src/app/components/accordion-control/accordion-control.component';
 import { AccordionComponent } from '@/src/app/components/accordion/accordion.component';
 import { ButtonComponent } from '@/src/app/components/button/button.component';
-import { TechRecordCreateRoutes } from '@/src/app/models/routes.enum';
-import { selectSectionState } from '@/src/app/store/technical-records';
+import { NumberPlateComponent } from '@/src/app/components/number-plate/number-plate.component';
+import { TagComponent, TagType } from '@/src/app/components/tag/tag.component';
+import { RootRoutes, TechRecordCreateRoutes } from '@/src/app/models/routes.enum';
+import { StatusCodes, VehicleTypes } from '@/src/app/models/vehicle-tech-record.model';
+import { DefaultNullOrEmpty } from '@/src/app/pipes/default-null-or-empty/default-null-or-empty.pipe';
+import { FormatVehicleTypePipe } from '@/src/app/pipes/format-vehicle-type/format-vehicle-type.pipe';
+import { selectSectionState, selectTechRecord } from '@/src/app/store/technical-records';
 import { NgTemplateOutlet } from '@angular/common';
-import { Component, inject } from '@angular/core';
+import { Component, OnInit, inject } from '@angular/core';
 import { ActivatedRoute, Router } from '@angular/router';
 import { Store } from '@ngrx/store';
 
@@ -12,17 +17,35 @@ import { Store } from '@ngrx/store';
 	selector: 'app-hydrate-new-vehicle-record-v2',
 	templateUrl: './hydrate-new-vehicle-record-v2.component.html',
 	styleUrls: ['./hydrate-new-vehicle-record-v2.component.scss'],
-	imports: [AccordionControlComponent, AccordionComponent, NgTemplateOutlet, ButtonComponent],
+	imports: [
+		AccordionControlComponent,
+		AccordionComponent,
+		NgTemplateOutlet,
+		ButtonComponent,
+		NumberPlateComponent,
+		DefaultNullOrEmpty,
+		TagComponent,
+		FormatVehicleTypePipe,
+	],
 })
-export class HydrateNewVehicleRecordV2Component {
+export class HydrateNewVehicleRecordV2Component implements OnInit {
 	store = inject(Store);
 	route = inject(ActivatedRoute);
 	router = inject(Router);
 
+	techRecord$ = this.store.selectSignal(selectTechRecord);
 	sectionStates$ = this.store.selectSignal(selectSectionState);
 
+	readonly TagType = TagType;
+	readonly VehicleTypes = VehicleTypes;
+	readonly StatusCodes = StatusCodes;
+
+	ngOnInit(): void {
+		this.handleEmptyEditingTechRecord();
+	}
+
 	onChange(): void {
-		this.router.navigate(['create']);
+		this.router.navigate([RootRoutes.CREATE_TECHNICAL_RECORD]);
 	}
 
 	onCancel(): void {
@@ -30,4 +53,10 @@ export class HydrateNewVehicleRecordV2Component {
 	}
 
 	onCreateNewRecord(): void {}
+
+	private handleEmptyEditingTechRecord(): void {
+		if (!this.techRecord$()) {
+			this.router.navigate([RootRoutes.CREATE_TECHNICAL_RECORD]);
+		}
+	}
 }
